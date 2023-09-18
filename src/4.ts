2553cc72dc3dba43200a616148913357b1731e6c
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-18 11:19:48 +0300

    all tasks done

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,58 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(door: boolean, key: Key) {
+    this.door = door;
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door === true) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(false, key);
+  }
+
+  openDoor(key: Key): boolean {
+    if (this.key === key) {
+      this.door = true;
+    } else {
+      this.door = false;
+    }
+    return this.door;
+  }
+}
+
+const key = new Key(10);
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +60,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
